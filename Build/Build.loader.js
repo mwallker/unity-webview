80c31b48f61720e485326b49b17f69e575fa9411
mwallker <vladimir.milanovich@gmail.com>
2025-12-17 22:32:31 +0100

    Add close and complete buttons

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 97859965,
+    wasmFileSize: 97859963,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
